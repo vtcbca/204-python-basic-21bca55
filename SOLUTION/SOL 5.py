c68eb6394aca949083cc22a55c8f40f1d07f6295
21bca55 <96609531+21bca55@users.noreply.github.com>
2022-03-10 11:13:55 +0530

    Add files via upload

---
+++ SOLUTION/SOL 5.py
@@ -0,0 +1,15 @@
+#WAS to enter any number and print from which number from 0 to 9 it is divisible.
+no1=0
+no2=1
+no3=2
+no4=3
+no5=4
+no6=5
+no7=6
+no8=7
+no9=8
+n=int(input("enter the number to be divided by:"))
+for i in range(no1,no2,no3,no4,no5,no5,no6,no7,no8,no9):
+    if(i%n==0):
+        print(i)
+        
