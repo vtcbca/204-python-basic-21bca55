b0b5de23803ec403c94cd90eec5af08bae089cdc
21bca55 <96609531+21bca55@users.noreply.github.com>
2022-03-10 11:13:07 +0530

    Add files via upload

---
+++ SOLUTION/SOL 3.py
@@ -0,0 +1,6 @@
+#WAS TO ENETR EVEN OR NOT
+a=int(input("enter the number"))
+if (a % 2) == 0:  
+      print("{0} is Even number".format(a))  
+else:
+      print("{0} is not Even Number".format(a))
